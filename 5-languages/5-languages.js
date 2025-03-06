e2ec8296809c9bc78cd2a30a2d21db6441c339cf
erretick <digrey@bk.ru>
2025-03-06 17:54:03 +0300

    Домашнее задание 5.9

---
+++ 5-languages/5-languages.js
@@ -0,0 +1,12 @@
+const lang = "EN"; // EN, RU
+
+switch (lang) {
+  case "EN":
+    console.log("Hello");
+    break;
+  case "RU":
+    console.log("Привет");
+    break;
+  default:
+    break;
+}
